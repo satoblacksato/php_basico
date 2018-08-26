2b3c661b39615f7c928b19f3ef45dc6b4b0a6703
ernesto.liberio@gmail.com <ernesto.liberio@gmail.com>
2018-08-26 13:08:39 -0500

    tabla

--- views/table_users.php
+++ views/table_users.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 	<title>USUARIOS</title>
+	<link rel="stylesheet" 
+	type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container-fluid">
